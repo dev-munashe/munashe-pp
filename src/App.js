6f600aa554a1f118ac5036feedef3037dd27443a
Brighton Tandabantu <brighton.tandabantu@fullstack.co.za>
2023-06-10 11:36:01 +0200

    Added footer for my site.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonial from './components/testimonials/Testimonial'
 import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Portfolio />
       <Testimonial />
       <Contact />
+      <Footer />
     </>
   )
 }
