8dda798fe9b889ffe56f281426d3b764b7b32351
Brighton Tandabantu <brighton.tandabantu@fullstack.co.za>
2023-06-10 10:47:54 +0200

    Initial Commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a target='_blank' href='https://novoresume.com/cv-templates' download className='btn'>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let`s Talk</a>
+    </div>
+  )
+}
+
+export default CTA
