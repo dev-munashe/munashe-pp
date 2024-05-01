4d83d88e2e678543cb120fca4503d4056b42d4ca
Munashe <munashethandabantu@gmail.com>
2024-05-01 11:40:02 +0200

    Attached a cv.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a target='_blank' href='https://novoresume.com/cv-templates' download className='btn'>Download CV</a>
+        <a target='_blank' href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let`s Talk</a>
     </div>
   )
